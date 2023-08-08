83c6c5dd354645db9a0f65800adcaa74c3fcb720
Lukas Müller <lukas.peterzell@gmail.com>
2023-08-08 09:55:16 +0200

    Fehlerbehebungen

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,3 +1,4 @@
+
 <template>
   <ion-page>
     <ion-header :translucent="true">
@@ -6,24 +7,21 @@
         <ion-button router-link="New">+</ion-button>
       </ion-toolbar>
     </ion-header>
-
     <ion-content :fullscreen="true">
       <div id="container">
         <ion-list>
-          <ion-item v-for="result in results" :key="result.id">
-                      <ion-label>{{ result.name }}</ion-label>
-                    </ion-item>
+          <ion-item v-for="contact in contacts" :key="contact.id">
+            <ion-label>{{ contact.name }}</ion-label>
+          </ion-item>
         </ion-list>
       </div>
     </ion-content>
   </ion-page>
 </template>
-
 <script>
 import { IonContent, IonHeader, IonItem, IonList, IonPage, IonTitle, IonToolbar, IonButton, useIonRouter, IonLabel } from '@ionic/vue';
 import { defineComponent } from 'vue';
 import { Contacts, PhoneType, EmailType } from '@capacitor-community/contacts';
-
 defineComponent({
   methods: {
     navigateToCreation() {
@@ -32,35 +30,35 @@ defineComponent({
     }
   },
 });
-
 export default {
     components: {
         IonButton,
         IonContent,
         IonHeader,
         IonItem,
+        IonLabel,
         IonList,
         IonPage,
         IonTitle,
         IonToolbar
     },
-    created() {
-        const retrieveListOfContacts = async() => {
-            const projection = {
-                name:true,
-                phones:true,
-                postalAddresses:true,
-            };
-
-            const result = awaitContacts.getContacts({
-                projection,
-            });
+    created() {       
+        const projection = {
+        name:true,
+        phones:true,
+        postalAddresses:true,
         };
+        const contacts = ref([]);
+        contacts = awaitContacts.getContacts({
+          projection,
+         });
+        
+    },
+    methods: {
+      
     }
 }
-
 </script>
-
 <style scoped>
 #container {
   text-align: center;
@@ -68,23 +66,17 @@ export default {
   left: 0;
   right: 0;
   top: 50;
-
 }
-
 #container strong {
   font-size: 20px;
   line-height: 26px;
 }
-
 #container p {
   font-size: 16px;
   line-height: 22px;
-
   color: #8c8c8c;
-
   margin: 0;
 }
-
 #container a {
   text-decoration: none;
 }
